e077990fbfd70a6e9ada6d0ea3cecc2b6cdb35eb
DuanRan <admin@duanxr.com>
2019-02-27 09:22:32 +0800

    Daily learning

---
+++ src/com/duanxr/leetcode/easy/NumberOfSegmentsInAString.java
@@ -0,0 +1,27 @@
+package com.duanxr.leetcode.easy;
+
+/**
+ * @author Duanran 2019/2/27 0027
+ */
+public class NumberOfSegmentsInAString {
+
+  class Solution {
+
+    public int countSegments(String s) {
+      char[] chars = s.toCharArray();
+      int count = 0;
+      boolean crossLetter = false;
+      for (int i = chars.length - 1; i >= 0; i--) {
+        if (chars[i] == ' ') {
+          if (crossLetter) {
+            count++;
+          }
+          crossLetter = false;
+        } else {
+          crossLetter = true;
+        }
+      }
+      return crossLetter ? count + 1 : count;
+    }
+  }
+}
